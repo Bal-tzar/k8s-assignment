2928e7006d04cac1ff86ccd8981f37f4ca9f80b9
Otto <ottodemare1@gmail.com>
2025-10-22 22:52:49 +0200

    changed name on TodoItem class

--- Database/MongoDB.cs
+++
@@ -1,9 +0,0 @@
-namespace KubernetesAssignment.Database
-{
-    public class MongoDB
-    {
-        public string TodoItem { get; set; }
-        public bool IsCompleted { get; set; }
-
-    }
-}
---
+++ Database/TodoItem.cs
@@ -0,0 +1,10 @@
+namespace KubernetesAssignment.Database
+{
+    public class TodoItem
+    {
+        public string Id { get; set; }
+        public string Task { get; set; }
+        public bool IsCompleted { get; set; }
+
+    }
+}
