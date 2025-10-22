7c4897ace302708d11a155435e44bcc48ae1954d
Otto <ottodemare1@gmail.com>
2025-10-22 22:56:09 +0200

    Moved Todo to correct place (Models)

---
+++ Models/Todo.cs
@@ -0,0 +1,10 @@
+namespace KubernetesAssignment.Models
+{
+    public class TodoItem
+    {
+        public string Id { get; set; }
+        public string Task { get; set; }
+        public bool IsCompleted { get; set; }
+
+    }
+}
